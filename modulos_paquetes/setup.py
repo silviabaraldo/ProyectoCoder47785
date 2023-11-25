88438126210db4fc911ad4bc6e4fffef692d229a
Silvia Baraldo <baraldo.silvia@gmail.com>
2023-11-25 18:26:47 -0300

    subiendo un archivo

---
+++ modulos_paquetes/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+name= "paquete1",
+version="1.0",
+description="pmi primer paquete distribuido"
+author="Silvia Baraldo"
+author_email= "baraldo.silvia@mimail.com"
+
+packages= ["paquete1"]
+)
