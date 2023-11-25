4431ce901a958df310f9ef5c73b6b5fa409d949b
Silvia Baraldo <baraldo.silvia@gmail.com>
2023-11-25 13:11:44 -0300

    guardar el archivo de primer_script.py

---
+++ primer_script.py
@@ -0,0 +1,19 @@
+from typing import clientec 
+import sys
+argumentos: List [str] =sys.argv 
+
+print (argumentos)
+
+nombre = argumentos [1]
+email = argumentos [2]
+telefono = argumentos [3]
+pago = argumentos [4]
+tipo = argumentos [5]
+
+print(nombre)
+print(email)
+print(telefono)
+print(pago)
+print(tipo)
+
+
